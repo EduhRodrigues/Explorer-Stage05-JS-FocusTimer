ca51b29337fd13f6e8f0336bb201cd61ae9c35b3
Jorge Eduardo Rodrigues <eduardorodrigues23@gmail.com>
2022-04-25 23:50:26 -0300

    EventListener 'mouseover' added

---
+++ script.js
@@ -0,0 +1,48 @@
+// Forest //
+document.getElementById("forest-svg").addEventListener("mouseover", mouseOverForestSVG);
+document.getElementById("forest-svg").addEventListener("mouseout", mouseOutForestSVG);
+
+
+function mouseOverForestSVG() {
+  document.getElementById("forest-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+}
+
+function mouseOutForestSVG() {
+  document.getElementById("forest-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+}
+
+// Rain //
+document.getElementById("rain-svg").addEventListener("mouseover", mouseOverRainSVG);
+document.getElementById("rain-svg").addEventListener("mouseout", mouseOutRainSVG);
+
+function mouseOverRainSVG() {
+  document.getElementById("rain-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+}
+
+function mouseOutRainSVG() {
+  document.getElementById("rain-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+}
+
+// Coffee Shop //
+document.getElementById("coffeeShop-svg").addEventListener("mouseover", mouseOverCoffeeShopSVG);
+document.getElementById("coffeeShop-svg").addEventListener("mouseout", mouseOutOverCoffeeShopSVG);
+
+function mouseOverCoffeeShopSVG() {
+  document.getElementById("coffeeShop-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+}
+
+function mouseOutOverCoffeeShopSVG() {
+  document.getElementById("coffeeShop-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+}
+
+// FirePlace //
+document.getElementById("firePlace-svg").addEventListener("mouseover", mouseOverFirePlaceSVG);
+document.getElementById("firePlace-svg").addEventListener("mouseout", mouseOutOverFirePlaceSVG);
+
+function mouseOverFirePlaceSVG() {
+  document.getElementById("firePlace-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+}
+
+function mouseOutOverFirePlaceSVG() {
+  document.getElementById("firePlace-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+}
