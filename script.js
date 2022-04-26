4cf76763d9d104c37a72c9a4141d539840ff5821
Jorge Eduardo Rodrigues <eduardorodrigues23@gmail.com>
2022-04-26 01:17:30 -0300

    svg of sounds options ok

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById("teste").style.fill= "hsla(240, 6%, 21%, 1)"
+document.getElementById("forestPath").style.fill= "hsla(240, 6%, 21%, 1)"
 
  // Fill //
-document.getElementById("teste").addEventListener("mouseover", mouseOverForestSVG);
-document.getElementById("teste").addEventListener("mouseout", mouseOutForestSVG);
+document.getElementById("forestPath").addEventListener("mouseover", mouseOverForestSVG);
+document.getElementById("forestPath").addEventListener("mouseout", mouseOutForestSVG);
 
 
 // Forest //
@@ -12,24 +12,27 @@ document.getElementById("forest-svg").addEventListener("mouseout", mouseOutFores
 
 function mouseOverForestSVG() {
   document.getElementById("forest-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
-  document.getElementById("teste").style.fill= "hsl(0,0%,100%)"
+  document.getElementById("forestPath").style.fill= "hsl(0,0%,100%)"
 }
 
 function mouseOutForestSVG() {
   document.getElementById("forest-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
-  document.getElementById("teste").style.fill= "hsla(240, 6%, 21%, 1)"
+  document.getElementById("forestPath").style.fill= "hsla(240, 6%, 21%, 1)"
 }
 
 // Rain //
 document.getElementById("rain-svg").addEventListener("mouseover", mouseOverRainSVG);
 document.getElementById("rain-svg").addEventListener("mouseout", mouseOutRainSVG);
 
+
 function mouseOverRainSVG() {
   document.getElementById("rain-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+  document.getElementById("rainPath").style.fill= "hsl(0,0%,100%)"
 }
 
 function mouseOutRainSVG() {
   document.getElementById("rain-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+  document.getElementById("rainPath").style.fill= "hsla(240, 6%, 21%, 1)"
 }
 
 // Coffee Shop //
@@ -38,10 +41,12 @@ document.getElementById("coffeeShop-svg").addEventListener("mouseout", mouseOutO
 
 function mouseOverCoffeeShopSVG() {
   document.getElementById("coffeeShop-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+  document.getElementById("coffeeShopPath").style.fill= "hsl(0,0%,100%)"
 }
 
 function mouseOutOverCoffeeShopSVG() {
   document.getElementById("coffeeShop-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+  document.getElementById("coffeeShopPath").style.fill= "hsla(240, 6%, 21%, 1)"
 }
 
 // FirePlace //
@@ -50,8 +55,10 @@ document.getElementById("firePlace-svg").addEventListener("mouseout", mouseOutOv
 
 function mouseOverFirePlaceSVG() {
   document.getElementById("firePlace-svg").style.backgroundColor= "hsl(193.9,97.5%,31.2%)";
+  document.getElementById("firePlacePath").style.fill= "hsl(0,0%,100%)"
 }
 
 function mouseOutOverFirePlaceSVG() {
   document.getElementById("firePlace-svg").style.backgroundColor = "hsl(240,9.1%,89.2%)";
+  document.getElementById("firePlacePath").style.fill= "hsla(240, 6%, 21%, 1)"
 }
